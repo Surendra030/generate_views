656195afb67c69bb4f934a19328128f175c4c44d
Surendra030 <surendrababun030@gmail.com>
2025-01-30 16:13:44 +0530

    kubu 32

--- file_run.py
+++ file_run.py
@@ -29,35 +29,35 @@ file_name = 'encrypted_data.json'
 data = decrypt_json(file_name,keypass)
 
 try:
-        
-    for index,url in enumerate(data):
-        try:
-                
-            if index%20==0:
-                driver.quit()
-                time.sleep(15)
-                service = Service(executable_path=chromedriver_path)
-                driver = webdriver.Chrome(service=service, options=options)
-                
-
-            driver.get(url)
-            time.sleep(2)
-            body = driver.find_element("tag name", "body")  # Focus on the body
-
-            # Wait for the page to load
-            for i in range(4):
-                body.send_keys(Keys.SHIFT, ">")
-                time.sleep(1)
-                
-            for i in range(35):
+     while(True):   
+        for index,url in enumerate(data):
+            try:
                     
-                # Send the right arrow key or 'L' key
-                body.send_keys(Keys.ARROW_RIGHT)  # Simulate right arrow key
+                if index%20==0:
+                    driver.quit()
+                    time.sleep(15)
+                    service = Service(executable_path=chromedriver_path)
+                    driver = webdriver.Chrome(service=service, options=options)
+                    
+
+                driver.get(url)
                 time.sleep(2)
-            time.sleep(10)
+                body = driver.find_element("tag name", "body")  # Focus on the body
+
+                # Wait for the page to load
+                for i in range(4):
+                    body.send_keys(Keys.SHIFT, ">")
+                    time.sleep(1)
+                    
+                for i in range(35):
+                        
+                    # Send the right arrow key or 'L' key
+                    body.send_keys(Keys.ARROW_RIGHT)  # Simulate right arrow key
+                    time.sleep(2)
+                time.sleep(10)
+                
+            except Exception as e:
+                print("Error 59 :",e)
             
-        except Exception as e:
-            print("Error 59 :",e)
-        
 except Exception as e:
     print("Error 63 ",e)
